cfb23db0f3942a648c0f2c7a7baa1caba10b6005
Que1r <shadowkinglonkingnotdie@gmail.com>
2025-03-14 12:18:32 +0700

    Delete qr.js

--- qr.js
+++
@@ -1,9 +0,0 @@
-const QRCode = require('qrcode');
-
-async function generateQR(amount, phone) {
-    const data = `https://payment.example.com/qr?phone=${phone}&amount=${amount}`;
-    const qr = await QRCode.toDataURL(data);
-    return qr;
-}
-
-module.exports = generateQR;
